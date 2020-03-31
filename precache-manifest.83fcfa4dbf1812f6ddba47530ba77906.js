dd0f3380b433f53de901755dd265663fab8b1e66
ennshi <enn.shi@gmail.com>
2020-03-31 16:15:42 +0200

    Updates

---
+++ precache-manifest.83fcfa4dbf1812f6ddba47530ba77906.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac1a41b16ac55103c3230337e1c7624",
+    "url": "/react-workshop/index.html"
+  },
+  {
+    "revision": "ad67b256353165996177",
+    "url": "/react-workshop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "513696dd9f3b6da4b311",
+    "url": "/react-workshop/static/css/main.1d0537e6.chunk.css"
+  },
+  {
+    "revision": "ad67b256353165996177",
+    "url": "/react-workshop/static/js/2.629b93fc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-workshop/static/js/2.629b93fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513696dd9f3b6da4b311",
+    "url": "/react-workshop/static/js/main.0b897243.chunk.js"
+  },
+  {
+    "revision": "1ec225071cae28d744d6",
+    "url": "/react-workshop/static/js/runtime-main.f695dc37.js"
+  }
+]);
